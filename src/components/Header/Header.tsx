16eeb368f893100a5caf4b97410851b85893fb35
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-08-15 22:35:46 +0500

    Bootstrapped Quiz App with Typescript

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Grid} from "@material-ui/core"
+
+const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Header
